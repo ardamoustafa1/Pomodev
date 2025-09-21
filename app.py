7e420d7201e411ee5611af247d38221afd02bc33
ardamoustafa1 <ardamoustafa@hotmail.com>
2025-09-22 00:06:20 +0300

    Netlify deployment configuration added

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-
-
+import os
 
 app = Flask(__name__)
 
@@ -8,5 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/static/<path:filename>')
+def static_files(filename):
+    return app.send_static_file(filename)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
